4abca134e9cf212da4bff6f3a1eaaf1c6fad83d7
Mahmut Bilir <mahmut-bilir@hotmail.com>
2024-10-27 21:43:24 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+var redis = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+var db = redis.GetDatabase();
+
+
+string key = "my_key";
+string value = "Hello, Redis!";
+await db.StringSetAsync(key, value);
+Console.WriteLine($"Added: {key} -> {value}");
+
+string retrievedValue = await db.StringGetAsync(key);
+
+Console.WriteLine($"Retrieved value: {key} -> {retrievedValue}");
+
+await redis.CloseAsync();
